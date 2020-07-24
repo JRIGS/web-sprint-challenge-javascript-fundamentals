42cbe966ffca1e4852b0ac9c5f430e2fb9947428
JRIGS <raczkowskijonathan@gmail.com>
2020-07-24 13:48:14 -0400

    Polished counter function

--- challenges/closure.js
+++ challenges/closure.js
@@ -31,7 +31,7 @@ For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
   function summation(num){
     let count = 0;
     for(let i = 1; i <= num; i++){
-      count += num
+      count += i
     }
     return count
   } 
